3a686315f2ceadd34363da8f667740f9ce91449a
Diana Olano <1005967885@u.icesi.edu.co>
2021-05-01 22:39:34 -0500

    javadoc of the Main class

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,13 @@
 package ui;
 
 public class Main {
+    
   public static void main(String[] args) {
-    Menu menu;
-    menu = new Menu();
+    /**
+     * Pre: An object of the menu class is created to navigate the program
+     * Pos: The game is initialized correctly by means of the menu object
+     */
+    Menu menu= new Menu();
     menu.startProgram();
   }
 }
